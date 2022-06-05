69621e08805191b3857be2966889bfd027a43b24
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-05 15:14:47 -0700

    try to fix subtotal in dishes.ejs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,50 @@
 // Client facing scripts here
 
+$(document).ready(function() {
+  $("#add-to-order").on("click", function() {
+    console.log("add to order CLICK ====");
+  })
+
+  $(".item-price").each(function (index) {
+    console.log("INDEX:", index + "\n  PRICE:" + $(this).text());
+  });
+
+  // $(".item-quantity").val()(function (quantity) {
+  //   console.log("QUANTITY: ", quantity + $(this).text());
+  // });
+
+
+$(".item-quantity")
+let id = $(this).attr(".item-quantity")
+  .keyup(function() {
+    var value = $( this ).val();
+    $("RAF WAS HERE", id).text( value );
+  })
+  .keyup();
+
+
+
+});
+
+$(function () {
+  $("#item-quantity").change(function () {
+    $(".place-my-order-subtotal").val(
+      parseInt($(this).val()) * parseInt($("#obj.id").val())
+    );
+  });
+});
+
+
+
+// $document.ready(function () {
+//  $
+// });
+
+// calculate the subtotal of my order
+// iterate over each menu item
+  // calculate price vs quantity
+// sum the iterations
+
+
+// how we access the values of each menu item
+
