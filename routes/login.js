c95323445e51ceeb6ff8b18b0f84de10f5e9bbb3
Ali <alihbouran@gmail.com>
2022-06-04 20:16:36 -0700

    Add login function

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const cookieSession = require('cookie-session');
+
+
+
+
+
+
 
 module.exports = (db) => {
   // GET /login/
@@ -7,11 +14,30 @@ module.exports = (db) => {
     res.render("login");
   });
 
- router.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+ // POST /login/
+ router.post("/", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
+  db.query(`SELECT email, password, users.id
+  FROM users
+  WHERE email = $1
+  AND password = $2;`, [email, password])
+  .then(data => {
+
+    let loginData = data.rows
+    let loginEmail = loginData[0].email
+    let loginPassword = loginData[0].password
+    let loginId = loginData[0].id
+
+
+    if (loginEmail === email && loginPassword === password) {
+        req.session.userId = loginId;
+        return res.redirect('/home')
+    }
+    res.send({error: "error"});
+        return;
+   })
  })
-
 return router;
 };
