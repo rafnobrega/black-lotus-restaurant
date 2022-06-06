04c446f5e17f4ed4a093f942b27e0b761c22f8f9
puneet2121 <bedipuneet21@gmail.com>
2022-06-06 04:51:56 +0000

    Add additional styles to the profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,10 @@ const router = express.Router();
 module.exports = (db) => {
   //get request for profile page
   router.get('/' ,(req,res) => {
-    db.query(`SELECT dishes.title, dishes_orders.dish_id,total_price,notes,orders.user_id,dishes.price,orders.tip,orders.taxes,users.name,dishes_orders.order_id,dishes_orders.quantity
+    db.query(`SELECT SUM(dishes.price * dishes_orders.quantity) AS sum,dishes.title, dishes_orders.dish_id,total_price,
+    notes,orders.user_id,dishes.price,orders.tip,orders.taxes,users.name,
+    dishes_orders.order_id,dishes_orders.quantity,user_id,dishes.price * dishes_orders.quantity as amount
+    
     FROM dishes
     JOIN dishes_orders
     ON dishes_orders.dish_id = dishes.id
@@ -13,11 +16,14 @@ module.exports = (db) => {
     ON orders.id = order_id
     JOIN users
     ON users.id = user_id
-    WHERE orders.user_id = $1;
+    WHERE orders.user_id = $1
+    GROUP BY dishes.title,dish_id,
+    dishes_orders.dish_id,orders.total_price,orders.notes,
+    orders.user_id,dishes.price,orders.tip,orders.taxes,
+    dishes_orders.order_id,dishes_orders.quantity,users.name;
     `,[1]).then ((response) => {
       let users = response.rows
       let templateVars = {users};
-
       console.log('users', users)
       res.render('profile',templateVars);
     });
