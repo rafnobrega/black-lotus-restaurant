5bfa430e78b01a9db102b59b7d24b456a06e725f
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-06 15:51:43 -0700

    fix profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -4,11 +4,6 @@ const router = express.Router();
 module.exports = (db) => {
   //get request for profile page
   router.get('/' ,(req,res) => {
-<<<<<<< HEAD
-    db.query(`SELECT dishes.title, dishes_orders.dish_id,total_price,
-    notes,orders.user_id,dishes.price,orders.tip,orders.taxes,users.name,
-    dishes_orders.order_id,dishes_orders.quantity,user_id,dishes.price * dishes_orders.quantity as amount,orders.status
-=======
 
 
     db.query(`SELECT SUM(dishes.price * dishes_orders.quantity) AS sum,dishes.title, dishes_orders.dish_id,total_price,
@@ -16,7 +11,6 @@ module.exports = (db) => {
     dishes_orders.order_id,dishes_orders.quantity,user_id,dishes.price * dishes_orders.quantity as amount
 
 
->>>>>>> menu-button
     FROM dishes
     JOIN dishes_orders
     ON dishes_orders.dish_id = dishes.id
@@ -24,20 +18,6 @@ module.exports = (db) => {
     ON orders.id = order_id
     JOIN users
     ON users.id = user_id
-<<<<<<< HEAD
-    WHERE orders.user_id = $1;
-    `,[1]).then ((response) => {
-      let users = response.rows // rename to order
-      let sum = 0;
-      users.forEach(element => {
-        console.log("element",element.amount);
-        sum = sum + element.amount;
-      });
-      let countTax = sum * (13/100);
-      let totalAmount = countTax + sum + users[0].tip;
-      let templateVars = {users,sum,countTax,totalAmount};
-      // console.log('users', users)
-=======
     WHERE orders.user_id = $1
 
 
@@ -52,7 +32,6 @@ module.exports = (db) => {
       let templateVars = {users};
 
       console.log('users', users)
->>>>>>> menu-button
       res.render('profile',templateVars);
     });
 });
